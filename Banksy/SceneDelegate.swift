0cf55a289dce0497f2f9c4ca0942f3ee79a39d4d
d <d>
2023-03-29 21:31:55 +0300

    onBoarding

--- Banksy/SceneDelegate.swift
+++ Banksy/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = LoginVC()
+        //window?.rootViewController = LoginVC()
+        window?.rootViewController = OnboardingContainerViewController()
         window?.makeKeyAndVisible()
         
     }
