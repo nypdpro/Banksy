947468c48dc37c6681715e3230c9f97ad10be267
nypdpro <nypdpro@rambler.ru>
2023-04-13 19:28:26 +0300

    bell badge

--- Banksy/Components/ShakeyBellView.swift
+++ Banksy/Components/ShakeyBellView.swift
@@ -11,12 +11,16 @@ import UIKit
 class ShakeyBellView: UIView {
     
     let imageView = UIImageView()
+    let buttonView = UIButton()
+    
+    let heightButton: CGFloat = 16
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         style()
         layout()
+        setup()
     }
     
     required init?(coder: NSCoder) {
@@ -30,23 +34,37 @@ class ShakeyBellView: UIView {
     
     
     
+    private func setup() {
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_: )))
+        imageView.addGestureRecognizer(singleTap)
+        imageView.isUserInteractionEnabled = true
+        
+    }
     
     
-    
-    func style() {
+    private func style() {
         
         translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
         let image = UIImage(systemName: "bell.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
         imageView.image = image
         
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        buttonView.backgroundColor = .systemRed
+        buttonView.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        buttonView.layer.cornerRadius = heightButton/2
+        buttonView.setTitle("10", for: .normal)
+        buttonView.setTitleColor(.white, for: .normal)
+        
     }
     
     
     
-    func layout() {
+    private func layout() {
         
         addSubview(imageView)
+        addSubview(buttonView)
         
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
@@ -54,6 +72,56 @@ class ShakeyBellView: UIView {
             imageView.heightAnchor.constraint(equalToConstant: 24),
             imageView.widthAnchor.constraint(equalToConstant: 24)
         ])
-
+        
+        NSLayoutConstraint.activate([
+            buttonView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            buttonView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -9),
+            buttonView.widthAnchor.constraint(equalToConstant: 15),
+            buttonView.heightAnchor.constraint(equalToConstant: 15)
+        ])
+        
+    }
+    
+    
+    @objc func imageViewTapped(_ recognizer: UITapGestureRecognizer) {
+        shakeWith(duration: 1.0, angle: .pi/8, yOffset: 0.0)
+        
+    }
+    
+    private func shakeWith(duration: Double, angle: CGFloat, yOffset: CGFloat) {
+        let numberOfFrames: Double = 6
+        let frameDuration = Double(1/numberOfFrames)
+        
+        imageView.setAnchorPoint(CGPoint(x: 0.5, y: yOffset))
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, options: [],
+                                animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration*2,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration*3,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration*4,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration*5,
+                               relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform.identity
+            }
+        },
+                                completion: nil
+        )
     }
 }
